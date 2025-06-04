db8dd9c0059620aababf441debd5f3770973b468
Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>
2025-06-04 15:16:25 +0900

    build: Add a script for building Linux-DomU image
    Signed-off-by: Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>

---
+++ build_xen_domu-linux_agl_cluster.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+die () {
+    echo >&2 "$(basename "$0"):${BASH_LINENO[0]}: ERROR:" "$@" && exit 1
+}
+
+Usage() {
+    echo "Usage: $0 <target_board>"
+    echo "board list:"
+    echo "- h3ulcb-4x2g-kf (h3sk 8GB + kingfisher board)"
+    echo "- h3ulcb-4x2g-ab (h3sk 8GB + ccpf-sk board)"
+    echo "- salvator-xs-h3-4x2g (Salvator-XS with H3 8GB)"
+    echo ""
+}
+target="$1"
+if [ -z "$target" ]; then
+    target="h3ulcb-4x2g-kf"
+    echo "default target: $target"
+fi
+
+if [[ "$target" != "h3ulcb-4x2g-kf" ]] && 
+    [[ "$target" != "h3ulcb-4x2g-ab" ]] && 
+    [[ "$target" != "salvator-xs-h3-4x2g" ]]; then
+    echo "Error: This board is not supported: $target"
+    Usage; exit -1
+fi
+
+[ ! -f "./scripts/prepare_build_environment.sh" ] && die "no such file: ./scripts/prepare_build_environment.sh"
+source ./scripts/prepare_build_environment.sh
+
+moulin prod-devel-rcar-virtio.yaml \
+    --MACHINE "$target" \
+    --ENABLE_ANDROID no \
+    --ENABLE_DOMU yes \
+    --GRAPHICS binaries \
+
+[ ! -f "./scripts/tweak_build_ninja.sh" ] && die "no such file: ./scripts/tweak_build_ninja.sh"
+./scripts/tweak_build_ninja.sh
+
+# Cleanup build directory
+rm -rf firmware
+find ../common_data/sstate | grep xen: | xargs rm -r
+find ../common_data/sstate | grep arm-trusted-firmware: | xargs rm -r
+find ../common_data/sstate | grep core-image-thin-initramfs: | xargs rm -r
+
+ninja || die "build failed"
+ninja full.img.gz || die "build failed"
+cp full.img.gz /srv/tftp/
+
+mkdir -p firmware
+find ./yocto/build-domd/tmp/deploy/images/ -name "*.srec" | xargs cp -t firmware
+
